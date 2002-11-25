8396c8e67011013778784284f79a85f512d867e6
Matthias Köppe <mkoeppe@math.ucdavis.edu>
2002-11-25 10:17:37 +0000

    (find-file): Implement for SBCL; implement for all other platforms using CLOCC PORT.

--- kpathsea.lisp
+++ kpathsea.lisp
@@ -4,17 +4,36 @@
 
 (in-package :kpathsea)
 
+;; We use CLOCC PORT to run programs if we are not running in CMUCL or SBCL.
+#-(or cmu sbcl)
+(require "PORT")
+
 (defun find-file (name)
   #+cmu
   (let ((process
 	 (extensions:run-program "kpsewhich" (list (namestring name))
 				 :output :stream)))
-    (prog1
-	(parse-namestring 
-	 (read-line (extensions:process-output process) nil nil))
-      (extensions:process-close process)))
-  #-cmu
-  (error "FIND-FILE is not implemented"))
+    (let ((line (read-line (extensions:process-output process) nil nil)))
+      (prog1
+	  (and line
+	       (parse-namestring line))
+      (extensions:process-close process))))
+  #+sbcl
+  (let ((process
+	 (sb-ext:run-program "/usr/bin/env"
+			     (list "kpsewhich" (namestring name))
+			     :output :stream)))
+    (let ((line (read-line (sb-ext:process-output process) nil nil)))
+      (prog1
+	  (and line
+	       (parse-namestring line))
+      (sb-ext:process-close process))))
+  #-(or cmu sbcl)
+  (let ((stream (port:pipe-input "kpsewhich" (namestring name))))
+    (let ((line (read-line stream nil nil)))
+      (prog1
+	  (and line (parse-namestring line))
+	(port:close-pipe stream)))))
 
 
 #|
