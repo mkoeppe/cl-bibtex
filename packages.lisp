a8b3b21327c211af1ad059e556c88a2dc73e30ff
Matthias Köppe <mkoeppe@math.ucdavis.edu>
2002-12-11 21:33:09 +0000

    Allegro portability fix.  Suggested by Drew McDermott.

--- packages.lisp
+++ packages.lisp
@@ -4,34 +4,36 @@
 
 (defpackage :kpathsea
   (:use :common-lisp)
-  (:export "FIND-FILE"))
+  (:export #:find-file))
 
 (defpackage :bibtex-runtime
   (:use :common-lisp)
-  (:export "*BIB-MACROS*" "*BIB-DATABASE*" "*BIB-ENTRIES*"
-	   "*BIB-ENTRY*" "*BIB-PREAMBLE*" "*BIB-STYLE*"
-	   "*BIB-FILES*" "*CITE-ALL-ENTRIES*" "*CITE-KEYS*"
-	   "*BIB-ENTRY-TYPE-FUNCTIONS*" #:*min-crossrefs*
-	   "READ-AUX-FILE" "READ-BIB-DATABASE" "CITED-BIB-ENTRIES"
-	   "WRITE-BIB-ENTRY"
-	   "READ-ALL-BIB-FILES-AND-COMPUTE-BIB-ENTRIES"
-	   "BIBTEX-NAME" "MAKE-BIBTEX-NAME" "BIBTEX-NAME-FIRST"
-	   "BIBTEX-NAME-VON" "BIBTEX-NAME-LAST" "BIBTEX-NAME-JR"
-	   "*BIBTEX-SPLIT-INITIALS*" "PARSE-BIBTEX-NAME" "PARSE-BIBTEX-NAME-LIST"
-	   "*BIBTEX-LONG-TOKEN-LENGTH*" "*BIBTEX-LONG-NAME-LENGTH*"
-	   "FORMAT-BIBTEX-NAME"
-	   "FORMAT-NTH-BIBTEX-NAME" "NUM-BIBTEX-NAMES" "BIBTEX-SUBSTRING"
-	   "ADD-PERIOD-UNLESS-SENTENCE-END"
-	   "WHITESPACE-P" "EMPTY-FIELD-P" "BIBTEX-STRING-PURIFY"
-	   "BIBTEX-STRING-DOWNCASE" "BIBTEX-STRING-UPCASE" "BIBTEX-STRING-TITLEDOWNCASE"
-	   "BIBTEX-STRING-PREFIX" "BIBTEX-STRING-WIDTH"
-	   "BIB-WARN" "BIB-WARN*" "BIB-ERRROR" "BIB-FATAL"
+  #+allegro (:shadow #:variable)
+  (:export #:*bib-macros* #:*bib-database* #:*bib-entries*
+	   #:*bib-entry* #:*bib-preamble* #:*bib-style*
+	   #:*bib-files* #:*cite-all-entries* #:*cite-keys*
+	   #:*bib-entry-type-functions* #:*min-crossrefs*
+	   #:read-aux-file #:read-bib-database #:cited-bib-entries
+	   #:write-bib-entry
+	   #:read-all-bib-files-and-compute-bib-entries
+	   #:bibtex-name #:make-bibtex-name #:bibtex-name-first
+	   #:bibtex-name-von #:bibtex-name-last #:bibtex-name-jr
+	   #:*bibtex-split-initials* #:parse-bibtex-name #:parse-bibtex-name-list
+	   #:*bibtex-long-token-length* #:*bibtex-long-name-length*
+	   #:format-bibtex-name
+	   #:format-nth-bibtex-name #:num-bibtex-names #:bibtex-substring
+	   #:add-period-unless-sentence-end
+	   #:whitespace-p #:empty-field-p #:bibtex-string-purify
+	   #:bibtex-string-downcase #:bibtex-string-upcase #:bibtex-string-titledowncase
+	   #:bibtex-string-prefix #:bibtex-string-width
+	   #:bib-warn #:bib-warn* #:bib-error #:bib-fatal
 	   #:*err-count* #:*history*
 	   #:+spotless-history+ #:+warning-message+ #:+error-message+ #:+fatal-message+
 	   #:*bbl-output*))
 
 (defpackage :bibtex-compiler
   (:use :common-lisp :bibtex-runtime)
+  #+allegro (:shadow #:variable)
   (:export #:compile-bst-file #:bibtex
 	   #:*bibtex-styles* #:*allow-load-lisp-bibtex-style*
 	   #:register-bibtex-style #:define-bibtex-style #:find-bibtex-style))
